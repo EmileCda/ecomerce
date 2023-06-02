4420e7909430df78dc4417c9417fe9d1e49824f7
emileCda <emile.cda@gmail.com>
2023-06-02 16:16:50 +0200

    end of project week

--- src/main/java/fr/ecommerce/backingbean/AddCategoryBean.java
+++ src/main/java/fr/ecommerce/backingbean/AddCategoryBean.java
@@ -3,39 +3,82 @@ package fr.ecommerce.backingbean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import fr.ecommerce.Ctrl.implement.UserCtrl;
+import fr.ecommerce.Ctrl.interfaces.IUserCtrl;
 import fr.ecommerce.common.IConstant;
 import fr.ecommerce.entity.Categorie;
+import fr.ecommerce.enums.Profile;
+import fr.ecommerce.model.dao.implement.CategorieDao;
+import fr.ecommerce.model.dao.interfaces.ICategorieDao;
+import fr.ecommerce.utils.DataTest;
+import fr.ecommerce.utils.Utils;
 
 @ManagedBean
 @SessionScoped
 public class AddCategoryBean extends MasterBean implements IConstant {
-	
-	
-	
-	Categorie categorie ;
 
+	Categorie categorie;
 	
-	
-	
-	
-	
+	private boolean trueValue ;
+	private boolean falseValue ;
+
 	public AddCategoryBean() {
-		// TODO Auto-generated constructor stub
+		
+		this.setCategorie(new Categorie() );
+		this.setCategorie(DataTest.genCategorie());
+		this.setFalseValue(false);
+		this.setTrueValue(true);
 	}
 
+// %%%%%%%%%%%%%%%%%%%%%%%%%% action %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+	public String addCategorie() {
+		String landingWebPage = STOREKEEPER_HOME;
+		ICategorieDao categorieDao = new CategorieDao();
 
+		try {
+			categorieDao.addCategorie(this.getCategorie());
+		} catch (Exception e) {
+			Utils.trace("catch addcategorie %s\n", e.toString());
+			return HOME;
+		}
+		return landingWebPage;
+	}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%% action %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	public String cheatCode() {
+
+		this.setCategorie(DataTest.genCategorie());
+
+		if (this.getCategorie() != null)
+			Utils.trace("cheatCode user %s\n", this.getCategorie());
+		else
+			Utils.trace("cheatCode Categorie null \n");
+		return null;
+	}
+//--------------------------------------------------------------------------------
 	public Categorie getCategorie() {
 		return categorie;
 	}
 
-	
-	
 	public void setCategorie(Categorie categorie) {
 		this.categorie = categorie;
 	}
-	
-	
-	
+
+	public boolean isTrueValue() {
+		return trueValue;
+	}
+
+	public void setTrueValue(boolean trueValue) {
+		this.trueValue = trueValue;
+	}
+
+	public boolean isFalseValue() {
+		return falseValue;
+	}
+
+	public void setFalseValue(boolean falseValue) {
+		this.falseValue = falseValue;
+	}
 
 }
--- src/main/java/fr/ecommerce/backingbean/ItemManagementBean.java
+++ src/main/java/fr/ecommerce/backingbean/ItemManagementBean.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ValueChangeEvent;
 
 import fr.ecommerce.Ctrl.implement.UserCtrl;
 import fr.ecommerce.Ctrl.interfaces.IUserCtrl;
@@ -14,14 +15,19 @@ import fr.ecommerce.entity.Categorie;
 import fr.ecommerce.entity.User;
 import fr.ecommerce.model.dao.implement.CategorieDao;
 import fr.ecommerce.model.dao.interfaces.ICategorieDao;
+import fr.ecommerce.utils.Utils;
 
 @ManagedBean
 @SessionScoped
 public class ItemManagementBean extends MasterBean implements IConstant {
 
 	List<Categorie> categorieList;
+	int  categorieId ;
+	Categorie currentCategorie;
 
 	public ItemManagementBean() {
+		
+		
 		if (this.getCategorieList() == null) {
 			this.setCategorieList(new ArrayList<Categorie>());
 		}
@@ -31,6 +37,19 @@ public class ItemManagementBean extends MasterBean implements IConstant {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		if (this.getCategorieList().size()>0 ) {				// prendre l'id (par défaut) du premier de la liste 
+			this.setCategorieId(this.getCategorieList()
+								.get(1)
+								.getId());
+			
+		}else
+			this.setCategorieId(0);								// liste vide
+			
+		this.setCategorieId(5);								// liste vide
+		this.setCurrentCategorie( this.getCategorieList()
+									.get(this.getCategorieId()));
+		
 
 	}
 	//-------------------------------------------------------------------------------------------------	
@@ -59,6 +78,18 @@ public class ItemManagementBean extends MasterBean implements IConstant {
 		
 			return pageReturn;
 		}
+		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    action %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+		public String categorieChange(ValueChangeEvent e) {
+			String pageReturn = null ;
+			Utils.trace("categorieChange  : %s\n", this.getCategorieId());
+			Utils.trace("categorieChange  : %s\n", e.getNewValue().toString());
+			Utils.trace("categorieChange d : %d\n", e.getNewValue());
+			Utils.trace("categorieChange 2  : %s\n", this.getCategorieId());
+		
+			this.setPromptStatus(String.format("Id cat : %d", e.getNewValue()));
+			return pageReturn;
+		}
 	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	//-------------------------------------------------------------------------------------------------	
 
@@ -69,6 +100,23 @@ public class ItemManagementBean extends MasterBean implements IConstant {
 
 	public void setCategorieList(List<Categorie> categorieList) {
 		this.categorieList = categorieList;
+	}
+
+
+	public int getCategorieId() {
+		return categorieId;
+	}
+
+	public void setCategorieId(int categorieId) {
+		this.categorieId = categorieId;
+	}
+
+	public Categorie getCurrentCategorie() {
+		return currentCategorie;
+	}
+
+	public void setCurrentCategorie(Categorie currentCategorie) {
+		this.currentCategorie = currentCategorie;
 	}
 
 }
